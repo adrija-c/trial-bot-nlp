61a2517541bce7e12ec68d897922df67be213000
Adrija Chakraborty <adrija.chakraborty@thoughtworks.com>
2024-01-15 11:29:30 +0530

    Add . Load pdf and read

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 import streamlit as st
 from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
 
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text 
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size = 1000,
+        chunk_overlap = 200,
+        length_function = len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
 
 def main():
     load_dotenv()
@@ -11,8 +30,19 @@ def main():
 
     with st.sidebar:
         st.subheader("Your documents")
-        st.file_uploader("Upload your socuments here and click on 'Process'")
-        st.button("Process")
+        pdf_docs = st.file_uploader("Upload your socuments here and click on 'Process'", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                #Get pdf text
+                raw_text = get_pdf_text(pdf_docs)
+                st.write(raw_text)
+
+                #Get text chunks
+                text_chunks = get_text_chunks(raw_text)
+                st.write(text_chunks)
+
+                #create vector store
+
 
 
 if __name__== '__main__':
